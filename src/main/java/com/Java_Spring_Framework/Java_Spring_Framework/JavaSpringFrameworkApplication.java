ce69897e39be1d873a5f6a353d163512003430a6
Rashmi Jayawardhana <155569390+rashmiJayawardhana@users.noreply.github.com>
2025-03-23 01:14:09 +0530

    Generate project

---
+++ src/main/java/com/Java_Spring_Framework/Java_Spring_Framework/JavaSpringFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.Java_Spring_Framework.Java_Spring_Framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringFrameworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Java_Spring_Framework/Java_Spring_Framework/JavaSpringFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Java_Spring_Framework.Java_Spring_Framework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
